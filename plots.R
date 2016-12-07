346d98ecd520d1431c2edba812812dc83c9e5273
Baptiste Rouger <rouger.b@gmail.com>
2016-12-07 20:02:29 +0100

    Finishing the report

--- plots.R
+++ plots.R
@@ -1,62 +1,85 @@
 rm(list=ls())
 
-#library(Hmisc)
+library(Hmisc)
 library(ggplot2)
+
+######
+# Exercise 1
+######
 
 L <- c(58.33, 43.11, 55.41, 62.72, 59.83, 57.74, 61.66, 56.19, 55.50, 63.26)
 M <- c(28.93, 44.03, 32.38, 27.43, 30.82, 31.97, 24.57, 33.94, 34.71, 26.62)
 S <- c(12.74, 12.86, 12.21, 9.85, 9.35, 10.29, 13.77, 9.87, 9.79, 10.12)
 
+# We convert the data to a dataframe
 data1 <- data.frame(L,M,S)
 
-
+# plot of L against M
+#
 p <- ggplot(data1, aes(M,L))
 p = p + geom_point() + geom_smooth(method=lm)
+
 pdf("corr1.pdf")
 print(p)
 dev.off()
 
+# plot of S against M-L
+#
 p2 <- ggplot(data1, aes(M-L, S))
 p2 = p2 + geom_point() + geom_smooth(method=lm)
 pdf("corr2.pdf")
 print(p2)
 dev.off()
 
-#=============================================#
+######
+# Exercise 3
+######
 
+# We load the data and put it in a dataframe
 Bar1 = read.csv("Bar1.csv", header=FALSE)
 Bar2 = read.csv("Bar2.csv", header=FALSE)
 
 Bars = data.frame(Bar1[,2],Bar2[,2])
 
+
+# plot of the data of the first barometer against the second
+#
 p3 <- ggplot(Bars, aes(Bars$Bar1, Bars$Bar2))
 p3 = p3 + geom_point() + geom_smooth(method=lm) + labs(x = "Barometer 1", y="Barometer 2")
-
-p4 <- ggplot(Bar1, aes(Bar1[,2] ~ Bar1[,1]))
-p4 <- p4 + geom_line()
-
-pdf("Baro1.pdf")
-plot(Bar1[,2], type="l", main="", xlab="", ylab="")
-title(main = "Pressure for the 7 last days (Barometer 1)", xlab="Hours", ylab="Pressure (hPa)")
-dev.off()
-
-pdf("Baro2.pdf")
-plot(Bar2[,2], type="l", main="", xlab="", ylab="")
-title(main = "Pressure for the 7 last days (Barometer 2)", xlab="Hours", ylab="Pressure (hPa)")
-dev.off()
 
 pdf("corr3.pdf")
 print(p3)
 dev.off()
 
+
+# plot of the data of the first barometer against time
+#
+pdf("Baro1.pdf")
+plot(Bar1[,2], type="l", main="", xlab="", ylab="")
+title(main = "Pressure for the 7 last days (Barometer 1)", xlab="Hours", ylab="Pressure (hPa)")
+dev.off()
+
+# plot of the data of the second barometer against time
+#
+pdf("Baro2.pdf")
+plot(Bar2[,2], type="l", main="", xlab="", ylab="")
+title(main = "Pressure for the 7 last days (Barometer 2)", xlab="Hours", ylab="Pressure (hPa)")
+dev.off()
+
+
+# We load the data from the first barometer and the second thermometer for 30 days
 Bar130 <- read.csv("Bar130.csv", header=FALSE)
 Air2 = read.csv("Air2.csv", header=FALSE)
 
+# plot of the temperature data against time
+#
 pdf("Air2.pdf")
 plot(Air2[,2], type="l", main="", xlab="", ylab="")
 title(main = "Air temperature for the 30 last days", xlab="Hours", ylab="Air temperature (°C)")
 dev.off()
 
+# plot of the pressure of the first barometer against the temparute of the second thermometer
+#
 PA30 <- data.frame(Air2[,2],Bar130[,2])
 p5 <- ggplot(PA30, aes(Air2...2.,Bar130...2.))
 p5 <- p5 + geom_point() + geom_smooth(method=lm)
--- test.R
+++ test.R
@@ -1,69 +1,99 @@
 rm(list=ls())
 
-#library(Hmisc)
+library(Hmisc)
+
+
+######
+# Exercise 1
+######
 
 L <- c(58.33, 43.11, 55.41, 62.72, 59.83, 57.74, 61.66, 56.19, 55.50, 63.26)
 M <- c(28.93, 44.03, 32.38, 27.43, 30.82, 31.97, 24.57, 33.94, 34.71, 26.62)
 S <- c(12.74, 12.86, 12.21, 9.85, 9.35, 10.29, 13.77, 9.87, 9.79, 10.12)
 
-#ml <- rcorr(M,L)
-#print("ML")
-#print(ml)
+# We compute the M~L correlation
+ml <- rcorr(M,L)
+print("ML")
+print(ml)
 
-#mls <- rcorr(M-L, S)
-#print("MLS")
-#print(mls)
+#We compute the M-L~S correlation
+mls <- rcorr(M-L, S)
+print("MLS")
+print(mls)
+
+
+######
+# Exercise 2
+######
 
 A <- c(98.63, 99.57, 98.28, 98.38, 99.49, 101.03, 98.73, 99.87, 99.27, 100.61)
 B <- c(99.02, 98.48, 98.46, 97.70, 99.51, 98.36, 97.78, 99.85, 98.27, 97.91)
 
+# We compute the Student test. A and B have the same mean
 tab <- t.test(A,B)
 print("TAB")
 print(tab)
 
+# We compute the Student test. A has a greater mean than B
 tabg <- t.test(A,B, alternative="greater")
 print("TABg")
 print(tabg)
 
+# We compute the Student test. A has a mean equal to 100mg
 ta <- t.test(A, mu=100)
 print("TA")
 print(ta)
 
+# We compute the Student test. B has a mean equal to 100mg
 tb <- t.test(B, mu=100)
 print("TB")
 print(tb)
 
+# We compute the Student test. B has a greater mean than 100mg
 tbl <- t.test(B, mu=100, alternative="greater")
 print("TBl")
 print(tbl)
 
-#========================#
 
+######
+# Exercise 3
+######
+
+# We import the data of the two barometers (7d, first line removed in .csv)
 Bar1 <- read.csv("Bar1.csv", header=FALSE)
 Bar2 <- read.csv("Bar2.csv", header=FALSE)
 
+# We put the pressures in a dataframe
 Bars <- data.frame(Bar1[,2],Bar2[,2])
 
-#bar7 <- rcorr(Bars$Bar1,Bars$Bar2, type="spearman")
-#print("Bars7")
-#print(bar7)
+# We compute the Spearman correlation between the pressures of the 2 barometers
+bar7 <- rcorr(Bars$Bar1,Bars$Bar2, type="spearman")
+print("Bars7")
+print(bar7)
 
+# We fit a linear model to the correlation between the two barometers
 lmbar7 <- lm(Bars$Bar2 ~ Bars$Bar1)
+summary(lmbar7)
 
+# We import the data of the first barometer (30d, first line removed in .csv)
 Bar130 <- read.csv("Bar130.csv", header=FALSE)
 
+# We select only the last 23 days and put these in a dataframe
 Bar23 <- data.frame( Bar130[1:(length(Bar130[,2]) - length(Bars$Bar1)),2])
 
+# We compute the two samples Kolmogorov-Smirnov test
 t723 <- ks.test(as.numeric(unlist(Bar23)),as.numeric(unlist(Bars$Bar1)))
 print(t723)
 
-###
 
-Bar130 <- read.csv("Bar130.csv", header=FALSE)
+# We import the data of the second thermometer (30d, first line removed in .csv)
 Air2 = read.csv("Air2.csv", header=FALSE)
 
-#ap <- rcorr(Bar130[,2],Air2[,2])
-#print("AP")
-#print(ap)
+# We compute the correlation between the data of the barometer and the thermometer
+ap <- rcorr(Bar130[,2],Air2[,2])
+print("AP")
+print(ap)
 
+# We fit a linear model to the correlation between the barometer and the thermometer
 lmAP <- lm(Bar130[,2]~Air2[,2])
+summary(lmAP)
